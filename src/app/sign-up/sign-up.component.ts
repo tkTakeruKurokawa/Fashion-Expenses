42c665a0eeb0696de77dec08762ea2ec339afc69
tkTakeruKurokawa <kurokawa.takeru@gmail.com>
2020-06-04 20:46:12 +0900

    added applecation description to sign up page

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { SessionService } from '../service/session.service';
 
@@ -8,8 +8,36 @@ import { SessionService } from '../service/session.service';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
-
   sign_up_form: FormGroup;
+
+  bar_names = ["COMME des GARCONS", "YOHJI YAMAMOTO", "ISSEY MIYAKE"];
+  bar_values = [300000, 200000, 100000];
+  bar_options: any = {
+    responsive: true,
+    legend: {
+      position: "bottom",
+    },
+  };
+  background_color = [
+    'rgb(255, 99, 132, 0.65)',
+    'rgb(54, 162, 235, 0.65)',
+    'rgb(255, 206, 86, 0.65)',
+    'rgb(231, 233, 237, 0.65)',
+    'rgb(75, 192, 192, 0.65)',
+    'rgb(151, 187, 205, 0.65)',
+    'rgb(220, 220, 220, 0.65)',
+    'rgb(247, 70, 74, 0.65)',
+    'rgb(70, 191, 189, 0.65)',
+    'rgb(253, 180, 92, 0.65)',
+    'rgb(148, 159, 177, 0.65)'
+  ];
+  bar_legend = true;
+  bar_type = "pie";
+
+  canvas: any;
+  ctx: any;
+  @ViewChild('chart', { read: ElementRef, static: false }) chart: ElementRef;
+
 
   constructor(
     private fb: FormBuilder,
